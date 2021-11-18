5968e69778bdf08a59ac00260f8dd1d271a30e94
KingPrime69 <qripot@gaming.tech>
2021-11-18 15:03:18 +0100

    mise en forme page d'accueil

--- Quentin/js/script.js
+++ Quentin/js/script.js
@@ -1,14 +1,3 @@
-M.AutoInit();
-
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.parallax');
-  var instances = M.Parallax.init(elems, options);
-});
-
-// Or with jQuery
-
 $(document).ready(function(){
   $('.parallax').parallax();
 });
-
-M.AutoInit();
