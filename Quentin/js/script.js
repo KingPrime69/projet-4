790334d95e4ef10a483c033436e142d757d9298d
KingPrime69 <qripot@gaming.tech>
2021-11-18 23:10:17 +0100

    new design

--- Quentin/js/script.js
+++ Quentin/js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+$(document).ready(function(){
   $('.carousel').carousel();
+});
+
+var instance = M.Carousel.getInstance(elem);
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true
 });
